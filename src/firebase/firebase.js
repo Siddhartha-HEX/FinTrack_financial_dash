d0d3242d4e7b442bb2211ca778d8393a192e9da9
Siddhartha Kumar <siddhartha290604@gmail.com>
2026-05-20 13:34:36 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhcG5bpjRFJj6z7dfScHiEeaaO5qfuBOI",
-  authDomain: "fintrack-4b04b.firebaseapp.com",
-  projectId: "fintrack-4b04b",
-  storageBucket: "fintrack-4b04b.firebasestorage.app",
-  messagingSenderId: "36744749834",
-  appId: "1:36744749834:web:562b273e05f55b261950f3",
-  measurementId: "G-RPE0ZL0H0V",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export default app;
